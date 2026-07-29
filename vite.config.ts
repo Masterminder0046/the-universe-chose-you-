5b72e749ff7e017ea4ced44e35500b70c94bde58
Masterminder0046 <sheikmohamed0046@gmail.com>
2026-07-29 12:42:16 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/the-universe-chose-you-/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
